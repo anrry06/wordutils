72c6899a9daf36ad56dd823fca564918c1b55f9a
Henri MEUNIER <hmeunier@maggie.snxt.xyz>
2015-12-11 12:30:05 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var wordutils = {
+    phonex: require("@anrry/phonex"),
+    didumean: require("./lib/didumean.js"),
+    occur: require("./lib/occur.js"),
+    dicodb: require("./lib/dicodb.js"),
+    grammar: require("./lib/grammar.js")
+};
+
+module.exports = wordutils;
